abb4d095ebd5da9d7d7da2b9bbb84ee33e222f06
circa10a <caleblemoine@gmail.com>
2020-11-14 14:04:58 -0600

    #patch: update ci

--- examples/main.tf
+++ examples/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     mcbroken = {
-      source  = "local/provider/mcbroken"
+      source = "local/provider/mcbroken"
     }
   }
 }
@@ -13,32 +13,32 @@ data "mcbroken_cities" "all" {}
 
 // Data resource to get current outage percentage of a specific city
 data "mcbroken_city" "Dallas" {
-    city = "Dallas"
+  city = "Dallas"
 }
 
 // If specified city isn't found, returns -1
 data "mcbroken_city" "not_found" {
-    city = "not_found"
+  city = "not_found"
 }
 
 // Get national average of broken ice cream machines
 output "global_broken_average" {
-    value = data.mcbroken_cities.all.broken
+  value = data.mcbroken_cities.all.broken
 }
 
 // Get list of all cities and their outage percentage
 output "all_available_cities" {
-    value = data.mcbroken_cities.all.cities
+  value = data.mcbroken_cities.all.cities
 }
 
 // Get outage percentage of a specific city
 output "user_specified_city" {
-    value = data.mcbroken_city.Dallas.broken
+  value = data.mcbroken_city.Dallas.broken
 }
 
 // When user specified city isn't found, return -1
 output "user_specified_city_not_found" {
-    value = data.mcbroken_city.not_found.broken
+  value = data.mcbroken_city.not_found.broken
 }
 
 # Apply complete! Resources: 0 added, 0 changed, 0 destroyed.
